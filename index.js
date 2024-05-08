cfb3e8a00f5dbb3bb646aa1f29bd301fad484426
tfeysudi <gs1ethan696@hotmail.com>
2024-05-08 16:23:45 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function minimumTotal(triangle) {
-  const n = triangle.length;
-  const dp = triangle[n - 1];
-  for (let i = n - 2; i >= 0; i--) {
-    for (let j = 0; j <= i; j++) {
-      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
-    }
-  }
-  return dp[0];
+function isValidBST(root) {
+  return isValid(root, null, null);
+}
+function isValid(node, min, max) {
+  if (!node) return true;
+  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+    return false;
+  return (
+    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+  );
 }
